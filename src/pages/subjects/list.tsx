9713cd56bc5b7dd2858891128962071b116500ca
PunyaKris <krishnatreypunya9211@gmail.com>
2026-03-23 17:54:29 +0530

    fitst commit
    s On branch master

---
+++ src/pages/subjects/list.tsx
@@ -0,0 +1,89 @@
+import { Breadcrumb } from "@/components/refine-ui/layout/breadcrumb.tsx";
+import { ListView } from "@/components/refine-ui/views/list-view.tsx";
+import { Input } from "@/components/ui/input.tsx";
+import { useMemo, useState } from "react";
+import { Search } from "lucide-react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select.tsx";
+import { DEPARTMENT_OPTIONS } from "@/constance";
+import { CreateButton } from "@/components/refine-ui/buttons/create.tsx";
+import { Subject } from "@/types";
+import { useTable } from "@refinedev/react-table";
+import { ColumnDef } from "@tanstack/react-table";
+import { Badge } from "@/components/ui/badge.tsx";
+import { DataTable } from "@/components/refine-ui/data-table/data-table.tsx";
+
+const Subjectslist = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedDepartment, setSelectedDepartment] = useState("all");
+
+  const subjectTable = useTable<Subject>({
+    columns: useMemo<ColumnDef<Subject>[]>(
+      () => [
+        {
+          id: "code",
+          accessorKey: "code",
+          size: 100,
+          header: () => <p className="column-title ml -2">Code</p>,
+          cell: ({ getValue }) => <Badge>{getValue<string>()}</Badge>,
+        },
+      ],
+      []
+    ),
+    refineCoreProps: {
+      resource: "subjects",
+      pagination: { pageSize: 10, mode: "server" },
+      filters: {},
+      sorters: {},
+    },
+  });
+
+  return (
+    <ListView>
+      <Breadcrumb />
+      <h1 className="page-title"> Subjects</h1>
+      <div className="intro-row">
+        <p>Quick access to essential metrics and management tools. </p>
+        <div className="search-field">
+          <Search className={"search-icon"} />
+
+          <Input
+            type="text"
+            placeholder="Search by name ... "
+            className="pl-10 w-full"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        </div>
+
+        <div className="flex gap-2 w-full sm:w-auto">
+          <Select
+            value={selectedDepartment}
+            onValueChange={setSelectedDepartment}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder=" Filter by department" />
+            </SelectTrigger>
+
+            <SelectContent>
+              <SelectItem value="all">All Departments</SelectItem>
+              {DEPARTMENT_OPTIONS.map((department) => (
+                <SelectItem key={department.value} value={department.value}>
+                  {department.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <CreateButton />
+        </div>
+      </div>
+      <DataTable table={subjectTable} />
+    </ListView>
+  );
+};
+export default Subjectslist;
